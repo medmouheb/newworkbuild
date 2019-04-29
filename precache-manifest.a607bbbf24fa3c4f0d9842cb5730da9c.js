94d45f471bfc06aea68ef4369e8949e98356c36f
medmouheb <mohamedmouheb@gmail.com>
2019-04-29 15:02:38 +0100

    menu

---
+++ precache-manifest.a607bbbf24fa3c4f0d9842cb5730da9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "179bfebd89ac11256b83",
+    "url": "/static/js/main.b5c44d1f.chunk.js"
+  },
+  {
+    "revision": "e73d41458fa9dc8eccac",
+    "url": "/static/js/2.e22f3d29.chunk.js"
+  },
+  {
+    "revision": "179bfebd89ac11256b83",
+    "url": "/static/css/main.c34b0c93.chunk.css"
+  },
+  {
+    "revision": "e73d41458fa9dc8eccac",
+    "url": "/static/css/2.f3007dac.chunk.css"
+  },
+  {
+    "revision": "6286d7bd9116ab797e9d9872423be8ee",
+    "url": "/index.html"
+  }
+];
